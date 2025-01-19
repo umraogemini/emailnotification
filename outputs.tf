ea7d43e9339d30f956146dfaab28a1a62cc94690
umraogemini <umraogemini@gmail.com>
2025-01-19 19:40:16 +0530

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "log_metric_name" {
+  value = google_logging_metric.log_metric.name
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "notification_email" {
+  description = "Email for alert notifications"
+  type        = string
+}
