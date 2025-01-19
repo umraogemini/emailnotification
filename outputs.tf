beb0cfad633cfc4923f6448d9ff4a9be4a88c9db
umraogemini <umraogemini@gmail.com>
2025-01-19 19:56:05 +0530

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "log_metric_name" {
   value = google_logging_metric.log_metric.name
 }
+
+output "email_notification_channel" {
+  value = google_monitoring_notification_channel.email_channel.display_name
+}
+
+output "webhook_notification_channel" {
+  value = google_monitoring_notification_channel.webhook_channel.display_name
+}
